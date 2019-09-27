1d2e4e86bf03b81faa7307f9c6fdedd7a7f03645
Jordan Long <jordanleelong5@gmail.com>
2019-09-27 02:11:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,11 @@
 # Your Code Here
 def my_own_map(array)
-  array = []
+  final_array = []
   counter = 0
   while counter < array.size do
+    final_array.push(array[counter] * -1 )
+    counter += 1
+  end
+  final_array
     
   end
